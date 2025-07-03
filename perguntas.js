7bcb9080d44d187e62f2223a00b22a1fd0e0ae2b
marianarcw <rachow.mariana@escola.pr.gov.br>
2025-07-03 08:51:56 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
     'O que é Python?',
-    'O Python é uma linguagem de programação'
+    '0 Python é uma linguagem de programação'
 )
 
 criaCartao(
     'Geografia',
     'Qual a capital da França?',
-    'A capital da França é Paris'
+    'A capital da França é Paris',
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
+    'O que é uma função?'
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
-criaCartao(
-    'Lingua inglesa',
+CriaCartao(
+    'LIngua inglesa',
     'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Oi em ingles e HI(RAI)'
 )
