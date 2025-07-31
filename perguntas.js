eef8df803133bcc6c9e05dd54e75ffe7fd4a95da
marianarcw <rachow.mariana@escola.pr.gov.br>
2025-07-31 08:00:10 -0300

    SALVAR ALTERAÇÕES

--- perguntas.js
+++ perguntas.js
@@ -6,18 +6,18 @@ criaCartao(
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
+    'Qual a capital da França?'
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
+    'Programação'
+    'O que é uma função?'
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'lingua inglesa',
     'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Oi em inglêse Hi(RAI)'
 )
